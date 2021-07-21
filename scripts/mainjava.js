5c38d5edfb29383448dae2043a32277a8c3ed831
YamkelaMathontsi <82159030+YamkelaMathontsi@users.noreply.github.com>
2021-07-21 09:31:56 +0200

    Delete scripts directory

--- scripts/mainjava.js
+++
@@ -1,5 +0,0 @@
-function toggleNavbar() {
-  document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
-  // let navBar = document.querySelector("navbar-links");
-  // navBar.style.display = "flex";
-}
